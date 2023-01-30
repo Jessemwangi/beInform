c92d74aae8c348e1a69f6069430608c7986cea78
jessemwangi <jessejzee@gmail.com>
2023-01-30 23:50:45 +0200

    registration form

--- clientpoint/src/pages/Login.jsx
+++ clientpoint/src/pages/Login.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom'
 
 const Login = () => {
+    const initial ={
+        username:''
+        ,password:'',
+    }
+    const [inputs,SetInputs] = useState(initial)
+    const [err,setErr] =useState(null)
+    const navigate = Navigate();
+
+    const handleChange = (e) => {
+        
+        SetInputs(prev => ({...prev, [e.target.name]:e.target.value}))
+        console.log(inputs);
+    }
+    const signIn = async (e) =>{
+        e.preventDefault();
+     try {
+	  const res =await  axios.post('/auth/login',inputs)
+      navigate('/');
+	  console.log(res.data);
+
+} catch (error) {
+	console.log(error);
+   setErr(error.response.data)
+ 
+}
+    }
     return (
         <div className='auth'> 
             <h1>login</h1>
             <form action="">
-                <input type="text" placeholder='username' />
-                <input type="password" placeholder='passord' />
-                <button>Login</button>
-                <p>error message</p>
+                <input type="text" name='username' placeholder='username' onChange={handleChange}/>
+                <input type="password" name='password' placeholder='passord' onChange={handleChange} />
+                <button onClick={signIn}>Login</button>
+                {err && <p>{err}</p>}
                 <span>Dont have accout <Link to='/Register'>Register</Link> </span>
             </form> 
         </div>
--- clientpoint/src/pages/Register.jsx
+++ clientpoint/src/pages/Register.jsx
@@ -1,17 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import axios from 'axios'
 
 const Register = () => {
+    const initial ={
+        email:'',
+        username:''
+        ,password:'',
+    }
+    const [inputs,SetInputs] = useState(initial)
+    const [err,setErr] =useState(null)
+    const navigate = Navigate();
+
+    const handleChange = (e) => {
+        
+        SetInputs(prev => ({...prev, [e.target.name]:e.target.value}))
+        console.log(inputs);
+    }
+    const registerUser = async (e) =>{
+        e.preventDefault();
+     try {
+	  const res =await  axios.post('/auth/register',inputs)
+      navigate('/login');
+	  console.log(res.data);
+
+} catch (error) {
+	console.log(error);
+   setErr(error.response.data)
+ 
+}
+    }
+
     return (
         <div>
              <div className='auth'> 
             <h1>Register</h1>
             <form action="">
-                <input type="text" placeholder='username' />
-                <input type="password" placeholder='passord' />
-                <input type="email" placeholder='passord' required/>
-                <button>Login</button>
-                <p>error message</p>
+                <input type="text" placeholder='username' name='username' onChange={handleChange}/>
+                <input type="password" placeholder='password' name='password' onChange={handleChange} />
+                <input type="email" placeholder='email' name='email' required onChange={handleChange}/>
+                <button onClick={registerUser}>Login</button>
+                { err && <p>{err}</p>}
                 <span> have an accout <Link to='/login'>Login</Link> </span>
             </form> 
         </div>
